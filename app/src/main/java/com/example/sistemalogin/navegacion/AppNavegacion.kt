0d0ab8a7cb300007c26a76884adefb3996df98f1
JorgeRTorres <jorgetorres.0996@gmail.com>
2025-02-21 11:11:25 -0500

    finalizacion clase06

--- app/src/main/java/com/example/sistemalogin/navegacion/AppNavegacion.kt
+++ app/src/main/java/com/example/sistemalogin/navegacion/AppNavegacion.kt
@@ -2,8 +2,10 @@ package com.example.sistemalogin.navegacion
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun AppNavegacion(){
-
+    val navController = rememberNavController()
 }
